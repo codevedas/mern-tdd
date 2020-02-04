9dced491cf1b12e8ec8ae99bd73f0a818a7b0683
vjbaski <codevedas@gmail.com>
2020-02-04 12:48:55 +0000

    Step3. Further Tests: message has content, date and id

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,18 +1,23 @@
 import { expect } from "chai";
 import MessageApp from './app.js'
+
 describe("app", function() {
+
 let testApp = new MessageApp
+
   it("app has messages", function() {
     expect(testApp.messages).to.deep.equal([]);
   });
+
 it("app creates message (post)", function() {
     testApp.post('hi world')
     expect(testApp.messages.length).to.equal(1)
   });
+
 it("message has content, date, and id", function(){
-    expect(testApp.messages[1].content).to.equal("hi world")
-    expect(testApp.messages[1].date).not.to.equal(undefined)
-    expect(testApp.messages[1].id).to.equal(1)
+    expect(testApp.messages[0].content).to.equal("hi world")
+    expect(testApp.messages[0].date).not.to.equal(undefined)
+    expect(testApp.messages[0].id).to.equal(1)
   });
 it("app reads (get)", function() {
     expect(testApp.get(1).content).to.equal("hi world")
