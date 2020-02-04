2ec5656ad3f9679ada0b6b84f0652846dbcc08d6
vjbaski <codevedas@gmail.com>
2020-02-04 12:40:45 +0000

    Step3. Further Tests: post

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,14 @@ class MessageApp {
   constructor() {
     this.messages = []
   }
+  post(content){
+    let item = {
+      id: (this.messages.length + 1),
+      content: content,
+      date: new Date()
+    }
+    this.messages.push(item)
+    return this.messages
+  }
 }
 export default MessageApp
