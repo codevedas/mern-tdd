3caf988ac6319364e2e95c4ddd3044110af31604
vjbaski <codevedas@gmail.com>
2020-02-06 12:57:56 +0000

    Step 2. Express API Reads

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
-import express from 'express'
+import express from "express"
 const app = express()
-app.get('/', function (req, res) {
-   res.send({val: "Hello World"})
+import MessageApp from './lib/model.js'
+//hardcoded instance of message app vv
+let messageApp = new MessageApp("/\///json/\//testMessages.json")
+app.get('/', async (req, res) => {
+  return new Promise((resolve, reject) => {
+    let result = messageApp.getAll()
+      res.json(result)
+  })
 })
-app.listen(3001)
+app.listen(3001, function(){
+  console.log("Connected");
+})
 
 export default app
--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -2,11 +2,17 @@ import request from "supertest"
 import { expect } from "chai";
 import MessageApp from "../app.js"
 
-describe("Hello World test", function(){
-  it("first test", function(done) {
-    const res = request(MessageApp)
-    .get("/")
-    res.expect({val: "Hello World"})
-    res.expect(200, done)
-  })
+describe("MessageApp Test", function(){
+  it("gets all messages", function(done) {
+      const res = request(MessageApp)
+      .get("/")
+      res.expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err)
+        }
+        expect(res.body.length).to.equal(1)
+        done()
+      })
+    })
 })
