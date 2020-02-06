a246f826dba2f1304026f4b13876f264a4d0d728
vjbaski <codevedas@gmail.com>
2020-02-06 17:46:48 +0000

    Step 8. Cors

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ import express from "express"
 import routes from "./lib/routes.js"
 const app = express()
 import bodyParser from "body-parser"
+import cors from 'cors'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(bodyParser.json());
+app.use(cors());
 app.use(routes)
 
 app.listen(3001, function(){
